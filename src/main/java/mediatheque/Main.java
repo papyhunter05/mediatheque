e1dfe31a541f64e50dcd23f3065be74ad11bb700
Papy <suzannoirwinmaharavo@gmail.com>
2026-09-01 21:13:02 +0300

    Secind commit

--- src/main/java/mediatheque/Main.java
+++ src/main/java/mediatheque/Main.java
@@ -1,25 +1,77 @@
 package mediatheque;
 
 import java.time.LocalDate;
+import java.util.Scanner;
 
 import mediatheque.model.Adherent;
 import mediatheque.model.DVD;
 import mediatheque.model.Emprunt;
 import mediatheque.model.Etudiant;
 import mediatheque.model.Livre;
+import mediatheque.model.Banner;
+
 
 public class Main {
 
     public static void main(String[] args) {
-        Adherent etudiant = new Etudiant("Rakoto", "Fandresena", "E001", LocalDate.of(2025, 9, 1));
 
-        Livre livre = new Livre("Clean Code", "L001", "Robert C. Martin");
-        DVD dvd = new DVD("Le Fabuleux Destin d'Amelie Poulain", "D001", 122);
+        Banner banner = new Banner();
+        Scanner scanner = new Scanner(System.in);
+        boolean continuer = true;
 
-        Emprunt empruntLivre = new Emprunt(etudiant, livre, LocalDate.of(2026, 8, 1));
-        System.out.println(empruntLivre.traiterRetourEmprunt(LocalDate.of(2026, 8, 20)));
+        System.out.println("Bienvenue sur Mediatek");
+        banner.afficher();
 
-        Emprunt empruntDvd = new Emprunt(etudiant, dvd, LocalDate.of(2026, 8, 1));
-        System.out.println(empruntDvd.traiterRetourEmprunt(LocalDate.of(2026, 8, 5)));
+        while (continuer) {
+            System.out.println("""
+                    
+                    ================================
+                         MENU PRINCIPAL
+                    ================================
+                    
+                    1. Gérer les livres
+                    2. Gérer les adhérents
+                    3. Effectuer un emprunt
+                    4. Retourner un livre
+                    5. Voir les pénalités
+                    0. Quitter
+                    
+                    --------------------------------
+                    """);
+
+            System.out.print("Votre choix : ");
+            int choix = scanner.nextInt();
+
+            switch (choix) {
+                case 1:
+                    System.out.println("\n Gestion livre");
+                    break;
+
+                case 2:
+                    System.out.println("\n Gestion adherents");
+                    break;
+
+                case 3:
+                    System.out.println("\n Effectuer un emprunt");
+                    break;
+
+                case 4:
+                    System.out.println("\n Retourner un emprunt");
+                    break;
+
+                case 5:
+                    System.out.println("\n Les penalites");
+                    break;
+
+                case 0:
+                    System.out.println("\n Merci d'avoir utiliser Mediatek");
+                    break;
+
+                default:
+                    System.out.println("\n❌ Choix invalide !");
+            }
+        }
+
+
     }
 }
